45b541ff12b566c26cb0af69f69c5d220a86b769
Lido Paglia <lido@paglia.org>
2016-11-10 14:52:13 -0500

    ruby version bump

--- getruby.cmd
+++ getruby.cmd
@@ -16,10 +16,14 @@ echo No Ruby, need to get it!
 
 REM Get Ruby and Rails
 REM 64bit
-curl -o ruby224.zip -L https://bintray.com/artifact/download/oneclick/rubyinstaller/ruby-2.2.4-x64-mingw32.7z?direct
+rem curl -o ruby224.zip -L https://bintray.com/artifact/download/oneclick/rubyinstaller/ruby-2.2.4-x64-mingw32.7z?direct
+
+curl -o ruby225.zip -L https://dl.bintray.com/oneclick/rubyinstaller/ruby-2.2.5-i386-mingw32.7z?direct
+
+
 REM Azure puts 7zip here!
 echo START Unzipping Ruby
-SetLocal DisableDelayedExpansion & d:\7zip\7za x -xr!*.ri -y ruby224.zip > rubyout
+SetLocal DisableDelayedExpansion & d:\7zip\7za x -xr!*.ri -y ruby225.zip > rubyout
 echo DONE Unzipping Ruby
 
 REM Get DevKit to build Ruby native gems
